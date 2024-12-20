a8cda821d4f62bf017e97eef0c60cfd07540b8aa
shreyasling <shreyasling007@gmail.com>
2024-12-20 09:48:59 +0530

    changed profile image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profilePic from "../assets/edited.jpg";
+import profilePic from "../assets/shreyas.png";
 import { HERO_CONTENT } from "../constants";
 import { motion } from "framer-motion";
 
